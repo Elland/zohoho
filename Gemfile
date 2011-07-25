2cb87eed39c8dbb1dfd305fd96b55c529c86ed3c
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-07-25 00:29:30 -0300

    parsing the json so it's not as disgusting as zoho sends us

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "http://rubygems.org"
   gem 'httparty'
   gem 'json'
   gem 'xml-simple'
+  gem 'nokogiri'
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
--- lib/zohoho/recruit.rb
+++ lib/zohoho/recruit.rb
@@ -1,11 +1,13 @@
 module Zohoho 
   require 'httparty'
+  require 'nokogiri'
   require 'json'
 
   class Recruit
     include HTTParty
     
-    def initialize(username, password, apikey)
+    def initialize(username, password, apikey, type = 'json')
+      @type = type
       @conn = Zohoho::Connection.new 'zohopeople', username, password, apikey
     end
     
@@ -13,12 +15,31 @@ module Zohoho
       @conn.ticket_url
     end
     
-    def candidates_url(type = "json")
+    def candidates_url(type = @type)
       "http://recruit.zoho.com/ats/private/#{type}/Candidates/getRecords?apikey=#{@conn.api_key}&ticket=#{@conn.ticket}"
     end
     
-    def get_candidates(conditions = {:toIndex => 200})
-      self.class.get candidates_url, conditions
-    end    
+    def get_candidates(conditions = {})
+      (@type == 'json' ? JSON.parse(self.class.get(candidates_url, conditions)) : Nokogiri::XML.parse(self.class.get(candidates_url, conditions)))
+    end
+    
+    def candidates(conditions = {})
+      @candidates ||= get_candidates
+    end
+    
+    def get_parsed_candidates(conditions = {:toIndex => 200})
+      raw_candidates = candidates
+      @candidates = raw_candidates["response"]["result"]["Candidates"]["row"]
+      
+      #extracts the relevant data
+      @candidates.collect! do |candidate|
+        @hash = {}
+        candidate["FL"].collect do |pair|
+          pair = pair.to_a
+          @hash[pair.last.last.to_sym] = pair.first.last
+        end
+        @hash
+      end
+    end
   end
 end
